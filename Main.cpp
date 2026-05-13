286c29f42781a17a9a456fae086102b287fb55ba
RR-Ralefaso <rrralefaso@outlook.com>
2026-05-14 01:37:18 +0200

    validation

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include "src/Equations.hpp" //the equations ima use
 using namespace ATI;
+
 int main()
 {
     std::cout << "this program is used to take in audio (file/live) and convert it into frquencies then create an image based of each frequency. \n\n"
@@ -13,8 +14,16 @@ int main()
               <<"        ->Ps = total width of screen (pixels) / total length of song (seconds)"
               << std::endl;
 
-    //starting off by asking user if they want live audio or not
     Audiomapper mapper;
-    
+
+    // starting off by asking user if they want live audio or not
+    std::cout << "would you like to upload a file or live recording:\n"<< std::endl;
+    std::string response;
+    //taking in user inout with validation
+    do{
+        std::cout << "   for Live input [L]\n    for uploading input [u]" << std::endl;
+        getline(std::cin, response);
+    } while (response.empty());
+
     return EXIT_SUCCESS;
 }
