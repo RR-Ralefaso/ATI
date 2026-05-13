e06bf6f8bdb511626f6af6c104212152b8ae66cc
RR-Ralefaso <rrralefaso@outlook.com>
2026-05-13 20:15:38 +0200

    equations created

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    std::cout << "this program is used to take in audio (file/live) and convert it into frquencies then create an image based of each frequency. \n\n"
+              << "using the TOTAL DETAIL equation :\n    Total pixels = (L * Ps) * H \n"
+              << "       -> L  = length of song (seconds)\n"
+              << "       -> Ps = Pixels per second (yours tretch factor)\n"
+              << "       -> H  = Height"
+              <<"\n\n"
+              <<"        ->Ps = total width of screen (pixels) / total length of song (seconds)"
+              << std::endl;
+
+    return EXIT_SUCCESS;
+}
