26bdb5fa5aa4f8dc762fddcf5af6e1e6c189d4c7
RR-Ralefaso <rrralefaso@outlook.com>
2026-05-13 21:35:56 +0200

    completed headerfile

--- src/Equations.hpp
+++ src/Equations.hpp
@@ -1,20 +1,21 @@
 #ifndef EQUATIONS_HPP
 #define EQUATIONS_HPP
 
+#include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
 
+namespace ATI{
+    class Audiomapper{
 
+        std::string filepath; //path to file
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //functions declarations
+        int GetWidth();
+        int GetHeight();
+        double GetLength(const std::string &filepath);
+        double GetPixelsPerSecond();
+    };
+} // namespace for ATI
 #endif
