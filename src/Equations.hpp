362ea92ba688fd488354b0987bc16c1fa2a2503f
RR-Ralefaso <rrralefaso@outlook.com>
2026-05-14 19:27:25 +0200

    created ways to get frequencies based off the data

--- src/Equations.hpp
+++ src/Equations.hpp
@@ -3,8 +3,13 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <cmath>
+
+// SDL and Audio Libraries
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mixer.h>
+#include <fftw3.h>
 
 /*
 @author : Rothang Ralph Ralefaso
@@ -13,21 +18,46 @@
 @date : 2026
 */
 
-namespace ATI{
-    class Audiomapper{
+namespace ATI
+{
 
-        public:
+    class Audiomapper
+    {
+    public:
+        // Struct must be defined before the function signature using it
+        struct FrequencyPoint
+        {
+            double timestamp;
+            int frequencyHz;
+            float magnitude;
+        };
 
-            std::string filepath; //path to file
-            //functions declarations
-            int GetWidth();
-            int GetHeight();
-            
-            double GetLength(const std::string &filepath);
-            double GetPixelsPerSecond(std::string &filepath);
-            double GetTotalPixels(const std::string &filepath);
+        // Path to audio file
+        std::string filepath;
 
-        
+        // --- Screen & Dimension Helpers ---
+        int GetWidth();
+        int GetHeight();
+
+        // --- Audio Metadata ---
+        double GetLength(const std::string &filepath);
+
+        // --- Mapping Calculations ---
+        double GetPixelsPerSecond(const std::string &filepath);
+        double GetTotalPixels(const std::string &filepath);
+
+        // --- Core Frequency Analysis ---
+        /**
+         * Analyzes the audio using FFTW3 and returns a vector
+         * of magnitudes mapped to time and frequency.
+         */
+        std::vector<FrequencyPoint> GetFrequencyMap(const std::string &filepath);
+
+    private:
+        // Adding a private helper for internal calculations is often useful
+        // e.g., double sampleRate = 44100.0;
     };
-} // namespace for ATI
-#endif
+
+} // namespace ATI
+
+#endif // EQUATIONS_HPP
