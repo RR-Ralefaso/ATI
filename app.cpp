eea5c0bc5858b0de8f8ec55dfb0081b38de55ccf
RR-Ralefaso <rrralefaso@outlook.com>
2026-05-14 17:38:45 +0200

    updated app

--- app.cpp
+++ app.cpp
@@ -3,6 +3,11 @@
 #include "src/Equations.hpp" //the equations ima use
 using namespace ATI;
 
+
+
+
+
+
 int main()
 {
     std::cout << "this program is used to take in audio (file/live) and convert it into frquencies then create an image based of each frequency. \n\n"
@@ -11,22 +16,11 @@ int main()
               << "       -> Ps = Pixels per second (yours tretch factor)\n"
               << "       -> H  = Height"
               <<"\n\n"
-              <<"        ->Ps = total width of screen (pixels) / total length of song (seconds)"
+              <<"        ->Ps = total width of screen (pixels) / total length of song (seconds)\n\n"
               << std::endl;
 
     Audiomapper mapper;
 
-    // executing the equations files to use it
-    const char *command = "g++ -c src/Equations.cpp -o Equations`sdl2-config --cflags` -lSDL2 -lSDL2_mixer";
-    int status = std::system(command);
-    if (status == 0)
-    {
-        std::cout << "Successfully compiled to the parent folder!" << std::endl;
-    }
-    else
-    {
-        std::cerr << "Compilation failed." << std::endl;
-    }
 
     // starting off by asking user if they want live audio or not
     std::cout << "would you like to upload a file or live recording:\n"<< std::endl;
